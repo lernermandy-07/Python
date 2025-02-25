12efa0ea599b0ab6062334222d36fec3fe9aeea9
lernermandy-07 <jadhavmandy9070@gmail.com>
2025-02-25 17:15:00 +0530

    Add files via upload

---
+++ 3 operator.py
@@ -0,0 +1,258 @@
+#  python oprator
+
+# arithmetic operator
+
+# addition
+
+int.__add__(5677,787)
+
+r=77879+78789899
+
+print(r)
+
+f=78788989
+
+h=7989878655456676
+
+print(f+h)
+
+a=63383
+
+b=887777
+
+c=43522688
+
+print(a+b+c)
+
+a=(input('enter first no here :'))
+
+b=(input('enter second no here : '))
+
+c=(input('enter third no here :'))
+
+print("addition of 3 no is: ",(int(a)+int(b)+int(c)))
+
+# subtraction
+
+sub=34666-383883
+
+print(sub)
+
+a=70
+
+b=77
+
+print(a-b)
+
+int.__sub__(35,637)
+
+g=(input('enter first no :' ))
+
+h=(input('enter second no :'))
+
+i=(input('enter third no :'))
+
+print('the subtraction of this no is :' , int(g)-int(h)-int(i))
+
+# multiplication
+
+m=67888*6768787
+
+print(m)
+
+v=738838
+
+w=78989
+
+print(v*w)
+
+x=(input('enter first no :'))
+
+y=(input('enter second no :'))
+
+z=(input('enter third no : '))
+
+print('the multuplication of this no :',int(z)*int(y)*int(w))
+
+# division 
+
+t=90/9
+
+print(t)
+
+p=45
+
+q=10
+
+print(p/q)
+
+int.__truediv__(6,3)
+
+r=(input('enter first no :'))
+
+s=(input('enter second no: '))
+
+t=(input('enter third no :'))
+
+print('the division of this no : ' , int(r)/int(s)/int(t))
+
+# floor division 
+
+int.__floordiv__(45,9)
+
+v=78//13
+
+print(v)
+
+d=668
+
+e=8
+
+print(d//e)
+
+f=(input('enter the first no: '))
+
+g=(input('enter second no :'))
+
+h=(input('enter third no :'))
+
+print('the floor division of this no is : ', int(f)//int(g)//int(h))
+
+# modulus 
+
+l=788%78
+
+print(l)
+
+m=240
+
+n=3
+
+print(m%n)
+
+q=(input('enter first no :'))
+
+r=(input('enter second no :'))
+
+s=(input('enter third no :'))
+
+print('the modulus of the above no is :',int(q)%int(r)%int(s))
+
+# exponentiation
+
+b=5**5
+
+print(b)
+
+v=56
+
+n=77
+
+print(v**n)
+
+# e=(input('enter first no:'))
+
+# f=input('enetr second no :')
+
+# g=input('enter third no :')
+
+# print('the exponentiation of the no is :',int(e)**int(f)**int(g))
+
+# print(h*r+a+d/z-x**q%b//g)
+
+
+
+# i made a simple a calculator through input method
+
+cal_easy=input('enter first no:')
+
+cal_easy1=input('enter second no :')
+
+cal_easy2=input('enter third noo:')
+
+cal_easy3=input('enter fourth no :')
+
+print(int(cal_easy)+int(cal_easy3)+int(cal_easy2))
+
+print(int(cal_easy)*int(cal_easy2)*int(cal_easy2))
+
+print(int(cal_easy1)-int(cal_easy1)-int(cal_easy2))
+
+print(int(cal_easy)/int(cal_easy1)/int(cal_easy2))
+
+print(int(cal_easy1)+int(cal_easy2)*int(cal_easy)/int(cal_easy1)-int(cal_easy)*int(cal_easy3))
+
+# print(int((cal_easy)+int((cal_easy2))*int(cal_easy1)/int(cal_easy1)-int(cal_easy3)*int(cal_easy2)))
+
+
+# assignment operator 
+
+# =
+
+# +=
+
+# *=
+
+# /=
+
+
+
+# comparison operator
+
+# ==	Equal	x == y	
+
+# !=	Not equal	x != y	
+
+# >	Greater than	x > y	
+
+# <	Less than	x < y	
+
+# >=	Greater than or equal to	x >= y	
+
+# <=	Less than or equal to	x <= y	
+
+
+
+# bitwise operator in python
+
+# & 	AND	Sets each bit to 1 if both bits are 1
+
+# |	OR	Sets each bit to 1 if one of two bits is 1
+
+#  ^	XOR	Sets each bit to 1 if only one of two bits is 1
+
+# ~ 	NOT	Inverts all the bits
+
+# <<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+
+# >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+
+
+
+# logical oprator
+
+# and 	Returns True if both statements are true	x < 5 and  x < 10
+
+# or	Returns True if one of the statements is true	x < 5 or x < 4	
+
+# not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)	
+
+
+a=bin(24)
+
+print(a)
+
+
+
+
+# eval
+
+x=(input('enter your name :' ))
+
+y=int(input('enter the first  no :'))
+
+z=int(input('enter second no :'))
+
+print()
+
+x=eval(input('enter no here'))
